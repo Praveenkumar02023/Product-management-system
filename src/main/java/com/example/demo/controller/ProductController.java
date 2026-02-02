808146d0beda15086abaef6321a4889d2cdc8df3
Praveen <Praveenkumar042023@gmail.com>
2026-02-02 14:54:39 +0530

    add product controller , product model , product repository , product service.

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.service.ProductService;
+
+import com.example.demo.model.ProductModel;
+
+@RestController
 public class ProductController {
+
+    private ProductService productService = new ProductService();
+
+    @GetMapping("/products")
+    public List<ProductModel> getAllProducts() {
+        return productService.getAllProducts();
+    }
 
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,5 +1,15 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.model.ProductModel;
+import java.util.List;
+
 public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public List<ProductModel> getAllProducts() {
+        return productRepository.findAll();
+    }
 
 }
